e7138e7b39ffdf3ea8d44964ceddee5612ddf5f5
Emmanuel Michael <128823183+Reggie-Michael@users.noreply.github.com>
2024-04-19 20:34:55 +0000

    Update layout.js 2

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -4,13 +4,16 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import Loading from "../loading";
 import { Suspense } from "react";
+import { usePathname } from 'next/navigation'
 
 export default function Layout({ children }) {
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <Suspense fallback={<Loading />}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <section className="flex min-h-dvh  w-full items-center justify-center bg-[#3D6440] px-[3%] py-[2%] text-black md:px-[7.5%] lg:px-[10%]  ">
+        <section className={`flex ${pathname == "/home" ? "lg:min-h-dvh  h-dvh" : " h-dvh "}   w-full items-center justify-center bg-[#3D6440] px-[3%] py-[2%] text-black md:px-[7.5%] lg:px-[10%]`}>
           {children}
         </section>
       </ThemeProvider>
